7e0d02cbc2dea1e5fb99feb23cb14f196069d7d7
epik999 <95705410+epik999@users.noreply.github.com>
2022-09-17 16:40:16 +0100

    Add files via upload

---
+++ Problem 3.py
@@ -0,0 +1,12 @@
+# https://projecteuler.net/problem=3
+
+Number = 600851475143
+for x in range(2, Number+1) :
+    if Number%x == 0: 
+        IsPrime = True 
+        for y in range(2,(x//2 + 1)): 
+            if x % y == 0 :
+                IsPrime = False
+                break
+        if IsPrime == True:
+            print(x, end=' ')
