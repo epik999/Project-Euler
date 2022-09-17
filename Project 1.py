8838c8eaaf59346268350bef6101ffe684b68c43
epik999 <95705410+epik999@users.noreply.github.com>
2022-09-17 14:52:30 +0100

    Delete Project 1.py

--- Project 1.py
+++
@@ -1,8 +0,0 @@
-# https://projecteuler.net/problem=1
-
-Numbers = []
-NumberSum = 0
-for x in range(1,1000) :
-  if x%3 == 0 or x%5 == 0:
-    Numbers.append(x)
-print("The sum of all mutiples of 3 or 5 up to 1000 is",sum(Numbers))
