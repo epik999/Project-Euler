733fe958f3b1d3653f60d760e6d9e90db70db5af
epik999 <95705410+epik999@users.noreply.github.com>
2022-09-18 10:55:22 +0100

    Add files via upload

---
+++ Problem 7.py
@@ -0,0 +1,14 @@
+def PrimeCheck(Number) :
+    for x in range(2, Number) :
+        if Number % x == 0 :
+            return False
+    return True
+
+#FoundPrime = False
+Primes = 0
+i = 2
+while Primes < 10001:
+    if PrimeCheck(i) == True :
+        Primes += 1
+    i += 1
+print(i-1)
