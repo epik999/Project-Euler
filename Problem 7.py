5796d1c4cd3062a70056b50ea17c02e2785c318a
epik999 <95705410+epik999@users.noreply.github.com>
2022-09-18 18:16:46 +0100

    Add files via upload

--- Problem 7.py
+++ Problem 7.py
@@ -1,9 +1,32 @@
-def PrimeCheck(Number) :
-    for x in range(2, Number) :
-        if Number % x == 0 :
-            return False
-    return True
+from math import sqrt
 
+def PrimeCheck(Number) :
+    if Number > 2 and Number % 2 != 0 :
+        NumSqrt = round(sqrt(Number))
+        Index = 0
+        ExitLoop = False
+        IsPrime = True
+        while Index < len(PrimeList) and ExitLoop == False :
+            if PrimeList[Index] > NumSqrt :
+                ExitLoop = True
+                
+            elif Number % PrimeList[Index] == 0 :
+                ExitLoop = True
+                IsPrime = False
+            else :
+                Index += 1
+        if IsPrime :
+            PrimeList.append(Number)
+        return IsPrime
+    
+    elif Number != 2 :
+        return False
+    else :
+        return True
+        
+PrimeList = [2]
+Finished = False
+PrimeSum = 0
 Primes = 0
 i = 2
 while Primes < 10001:
